d3cd4bf70aa36e045232812f4c0d209e84bbd4b0
Giovanni Fiori <giovannicfiori@gmail.com>
2019-10-27 10:04:34 -0400

    Added core libs and basic styles

---
+++ src/components/Header/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 80px;
+    padding: 0 30px;
+    background: #3F51B5;
+    color: #fff;
+    user-select: none;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const UnorderedList = styled.ul`
+    display: flex;
+    align-items: center;
+
+`;
+
+export const ListItem = styled.li`
+    text-decoration: none;
+    margin: 0 18px;
+    
+`;
