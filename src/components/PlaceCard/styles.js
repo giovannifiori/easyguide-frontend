89e3e5f3a4e60a9813281ced357c050f79b0be3e
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-05 18:35:49 -0400

    Make accessibility message as a reusable component

--- src/components/PlaceCard/styles.js
+++ src/components/PlaceCard/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 500px;
+  width: 350px;
   margin: 16px 1px;
 `;
 
@@ -23,9 +23,4 @@ export const PlaceTitle = styled.h2`
 
 export const PlaceSubtitle = styled.p`
   color: #757575;
-`;
-
-export const Message = styled.p`
-  margin: 1% 0;
-  color: ${props => props.color || '#212121'};
 `;
