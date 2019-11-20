5cf43048db2313907ea7f69ab72e4a737aeb914c
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-20 14:52:29 -0400

    Adjustments in characteristic component

--- src/components/Characteristic/styles.js
+++ src/components/Characteristic/styles.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    margin: 12px 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 12px 0;
 `;
 
-export const Title = styled.h3`
-`;
+export const Title = styled.h3``;
 
 export const Description = styled.span`
-    margin-top: 4px;
+  margin: 4px 0;
 `;
