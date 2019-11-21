92debca784cfcb8f661ca26c667f6c620953854f
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-20 21:43:13 -0400

    sign in flow

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   width: 100%;
+  height: 100%;
   align-items: center;
   justify-content: center;
   flex-direction: column;
