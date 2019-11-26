b3092f616fa5f99f6400851b603bc80b8159f1bf
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-26 19:16:22 -0400

    Fixed app bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,37 +5,40 @@ import * as firebase from 'firebase/app';
 
 import { Container, UnorderedList, ListItem } from './styles';
 import AppLogo from '../../assets/img/logo.png';
+import { AppBar } from '@material-ui/core';
 
 export default function Header() {
   return (
-    <Container>
-      <img src={AppLogo} height="100%" alt="Easy Guide" />
-      <nav>
-        <UnorderedList>
-          {!!firebase.auth().currentUser ? (
-            <>
+    <AppBar position="fixed">
+      <Container>
+        <img src={AppLogo} height="100%" alt="Easy Guide" />
+        <nav>
+          <UnorderedList>
+            {!!firebase.auth().currentUser ? (
+              <>
+                <ListItem>
+                  <NavLink to="/search">Buscar</NavLink>
+                </ListItem>
+                <ListItem>
+                  <NavLink to="/nearby">Locais próximos</NavLink>
+                </ListItem>
+                <ListItem>
+                  <NavLink to="/favorites">Favoritos</NavLink>
+                </ListItem>
+                <ListItem>
+                  <NavLink to="/me">
+                    <PersonIcon />
+                  </NavLink>
+                </ListItem>
+              </>
+            ) : (
               <ListItem>
-                <NavLink to="/search">Buscar</NavLink>
+                <NavLink to="/signin">Login</NavLink>
               </ListItem>
-              <ListItem>
-                <NavLink to="/nearby">Locais próximos</NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink to="/favorites">Favoritos</NavLink>
-              </ListItem>
-              <ListItem>
-                <NavLink to="/me">
-                  <PersonIcon />
-                </NavLink>
-              </ListItem>
-            </>
-          ) : (
-            <ListItem>
-              <NavLink to="/signin">Login</NavLink>
-            </ListItem>
-          )}
-        </UnorderedList>
-      </nav>
-    </Container>
+            )}
+          </UnorderedList>
+        </nav>
+      </Container>
+    </AppBar>
   );
 }
