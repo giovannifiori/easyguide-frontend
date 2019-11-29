c95c178ae008ecd2d11ade19aaf62b96f4c06d42
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-29 19:24:03 -0400

    host config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,3 @@
-export const BASE_API_URL='http://localhost:3001/api/';
+export const SERVER_HOST = 'https://130.211.224.76';
+export const SERVER_PORT = 3001;
+export const BASE_API_URL = `${SERVER_HOST}:${SERVER_PORT}/api/`;
