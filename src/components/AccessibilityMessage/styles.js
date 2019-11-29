48cb6cb5af282b491cd56c74ba82b490e83a18db
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-29 19:23:48 -0400

    Adjusted font size

--- src/components/AccessibilityMessage/styles.js
+++ src/components/AccessibilityMessage/styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Message = styled.h3`
   margin: 1% 0;
-  font-size: ${props => props.fontSize ? `${props.fontSize}px` : '14px'}
+  font-size: ${props => (props.fontSize ? `${props.fontSize}px` : '1rem')}
   color: ${props => props.color || '#212121'};
 `;
