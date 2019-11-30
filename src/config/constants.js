502699c89ffa41455fde8d78e225069045907557
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-29 20:23:31 -0400

    fixed backend url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,1 @@
-export const SERVER_HOST = 'https://130.211.224.76';
-export const SERVER_PORT = 3001;
-export const BASE_API_URL = `${SERVER_HOST}:${SERVER_PORT}/api/`;
+export const BASE_API_URL = 'easyguide-backend.herokuapp.com';
