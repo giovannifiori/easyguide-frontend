2cbe3d62588d2251a36834d259b534cceba0c00a
Giovanni Fiori <giovannicfiori@gmail.com>
2019-11-30 20:36:24 -0400

    Handling add/remove favorites in local store

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -10,6 +10,24 @@ export const setFavorites = places => {
   };
 };
 
+export const addToFavorites = place => {
+  return {
+    type: actionTypes.ADD_TO_FAVORITES,
+    payload: {
+      place
+    }
+  };
+};
+
+export const removeFromFavorites = place => {
+  return {
+    type: actionTypes.REMOVE_FROM_FAVORITES,
+    payload: {
+      place
+    }
+  };
+};
+
 export const setSearchResults = places => {
   return {
     type: actionTypes.SET_SEARCH_RESULTS,
